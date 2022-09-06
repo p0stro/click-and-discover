f5529e997c2e298295fcfec20d0ef7cf46c1ccf9
p0stro <110983036+p0stro@users.noreply.github.com>
2022-09-06 10:48:45 +0200

    Create first version of "click and discover"

---
+++ index.js
@@ -0,0 +1,34 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
+canvas.addEventListener("click", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  drawCircle();
+});
+
+function drawCircle() {
+  ctx.fillStyle = "pink";
+  ctx.beginPath();
+  ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
+  ctx.save();
+  // ctx.fill();
+  ctx.clip();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.restore();
+  // ctx.clearRect(mouse.x - 30, mouse.y - 30, 60, 60);
+}
+//drawCircle();
